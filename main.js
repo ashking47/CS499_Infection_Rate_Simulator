2ac37a7b41678f65a4daaba950da621218479f8c
thelivingbrian <brian.lynch4444@gmail.com>
2017-06-05 16:46:33 -0400

    changed zoom level

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.8781, lng: -87.6298},
-        zoom: 10
+        zoom: 15
     });
     map.setOptions({draggable: false, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true});
 }
